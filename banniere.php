45ba60c5a5231503a12f34a875c6262063e11407
DmtBader <baderdeastance@gmail.com>
2024-01-30 15:04:07 +0100

    commit débauche

--- banniere.php
+++ banniere.php
@@ -1,29 +1,37 @@
         
         <section>
-            <Div><h1></h1></Div>
+            <a href="">
+                <Div><h1>CLUB AUTO</h1></Div>
+            </a>
+            
             <Div>
                 <img src="" alt="">
                 <div>
-                    <p></p>
-                    <p></p>
+                    <p>ADRESSE:</p>
+                    <p>AVENUE CHARLES DE GAULLE <br> 75000 PARIS</p>
                 </div>
             </Div>
             <Div>
                 <img src="" alt="">
                 <div>
-                    <p></p>
-                    <p></p>
+                    <p>TÉLÉPHONE:</p>
+                    <p>0123456789</p>
                 </div>
             </Div>
             <Div>
                 <img src="" alt="">
                 <div>
-                    <p><span></span></p>
-                    <p><span></span></p>
+                    <p>OUVERTURE: <span>Du Lundi au Samedi</span></p>
+                    <p>HORAIRE: <span>De 9h à 17h</span></p>
                 </div>
             </Div>
             <Div>
-                <img src="" alt="">
-                <img src="" alt="">
+                <a href="">
+                    <img src="" alt="">
+                </a>
+                <a href="">
+                    <img src="" alt="">
+                </a>
+                
             </Div>
         </section>
