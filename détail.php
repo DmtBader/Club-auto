8f17ea98272b2520b1a3debb4d9a0f7c619933d2
DmtBader <baderdeastance@gmail.com>
2024-01-30 11:21:05 +0100

    créat page

---
+++ détail.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>détail véhicule</title>
+    <link rel="stylesheet" href="Style/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Club auto</title>
+    <link rel="stylesheet" href="Style/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <section>
+            <Div></Div>
+            <Div></Div>
+            <Div></Div>
+            <Div></Div>
+            <Div></Div>
+        </section>
+        <nav>
+            <Div></Div>
+            <Div></Div>
+            <Div></Div>
+            <Div></Div>
+        </nav>
+
+        <nav>
+
+        </nav>
+    </header>
+
+    <main>
+        <section>
+            <Div></Div>
+            <Div></Div>
+        </section>
+           
+        <section>
+            <Div></Div>
+            <Div></Div>
+            <Div></Div>
+            <Div></Div>
+        </section>
+
+    </main>
+
+    <footer>
+
+    </footer>
+    
+</body>
+</html>
