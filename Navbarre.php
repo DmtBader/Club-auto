9959e403e07267abfbfaa17883cf57eab8d77300
DmtBader <baderdeastance@gmail.com>
2024-02-07 14:52:37 +0100

    css upgrade  la galère

--- Navbarre.php
+++ Navbarre.php
@@ -2,56 +2,36 @@
             include 'link.php';
         ?>
        
-       <nav class="bgGray op">
-            <a href="#">
-                <Div class="hov">
-                    <p class="ftBlack thovw">ACCUEIL</p>
-                </Div>
-            </a>
+       <nav class="bgGrayOp navH centerD inLine">
+            
+                <a href="#">
+                    <Div class="hov maxXW centerX2 hovWhite">
+                        <p class="ftBlack">ACCUEIL</p>
+                    </Div>
+                </a>
 
-            <a href="#">
-                <Div class="hov">
-                    <p class="ftBlack thovw">VENTE</p>
-                </Div>
-            </a>
+                <a href="#">
+                    <Div class="hov maxXW centerX2 hovWhite">
+                        <p class="ftBlack">VENTE</p>
+                    </Div>
+                </a>
 
-            <a href="#">
-                <Div class="hov">
-                    <p class="ftBlack thovw">LOCATION</p>
-                </Div>
-            </a>
+                <a href="#">
+                    <Div class="hov maxXW centerX2 hovWhite">
+                        <p class="ftBlack">LOCATION</p>
+                    </Div>
+                </a>
 
-            <a href="#">
-                <Div class="hov">
-                    <p class="ftBlack thovw">CONTACT</p>
-                </Div>
-            </a>
-                
+                <a href="#">
+                    <Div class="hov maxXW centerX2 hovWhite">
+                        <p class="ftBlack">CONTACT</p>
+                    </Div>
+                </a>
+            
         </nav>
 
-        <nav class="bgGray op">
-            <a href="#">
-                <Div>
-                    <p class="ftBlack">ACCUEIL</p>
-                </Div>
-            </a>
-
-            <a href="#">
-                <Div>
-                    <p class="ftBlack">VENTE</p>
-                </Div>
-            </a>
-
-            <a href="#">
-                <Div>
-                    <p class="ftBlack">LOCATION</p>
-                </Div>
-            </a>
-
-            <a href="#">
-                <Div>
-                    <p class="ftBlack">CONTACT</p>
-                </Div>
-            </a>
+        <nav>
+            <div>
                 
+            </div>
         </nav>
--- index.php
+++ index.php
@@ -20,27 +20,27 @@
                 include 'banniere.php';
             ?>
         </Div>
-        <Div class="imgHead"> 
+        <Div class="imgHead imgnav"> 
             <?php
                 include 'navbarre.php';
             ?>
-            
-            <Div class="bgBlack"><h2 class="ftwhite">CLUB AUTO <span class="ftOrange">LE MEILLEUR DE L'AUTOMOBILE</span></h2></Div>
         </Div>
+
+        <Div class="bgBlack contBan centerV"><h2 class="ftwhite">CLUB AUTO <span class="ftOrange">LE MEILLEUR DE L'AUTOMOBILE</span></h2></Div>
     </header>
 
     <main>
-        <section> <!--1ere section 4 conteneurs-->
-           <Div>
-                <Div> <img src="http://localhost/Clubauto/Media/clients.webp" alt="clients qui exprime leurs avec un geste de victoire devant un véhicule"></Div>
-                <Div>
+        <section class="contsec bgGray grid padsec2">
+           
+                <Div class="centerX2 z1"> <img class="imgcont2" src="http://localhost/Clubauto/Media/clients.webp" alt="clients qui exprime leurs avec un geste de victoire devant un véhicule"></Div>
+                <Div class="z2">
                     <h3>BIENVENU CHEZ <span class="ftOrange">CLUB AUTO</span></h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dolore dolorem voluptatibus!</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, at eligendi unde iure ipsum, sit, doloribus dolorem voluptatum quidem laborum quaerat cupiditate dolore assumenda in consequuntur. Labore quod consequuntur harum velit suscipit repellat, totam atque quasi nam quis fugit ut?</p>
                 </Div>
-           </Div>
-            <Div>
-                <Div>
+           
+            
+                <Div class="z3">
                     <h3>LE MEILLEUR DE L'AUTOMOBILE</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, magni. Nisi, sed libero laudantium accusamus minus tempore reprehenderit nam, doloribus dignissimos eaque cumque tenetur, quo consectetur dolores beatae voluptatibus aspernatur?</p>
                     <ul class="Puce ftOrange">
@@ -51,11 +51,11 @@
                         <li><span class="ftBlack">Lorem ipsum dolor sit amet.</span> </li>
                     </ul>
                 </Div>
-                <Div><img src="http://localhost/Clubauto/Media/entretien.webp" alt="technicien qui nettoie un véhicule avec précision, le regard dans le prolongement du véhicule"></Div>
-            </Div>
+                <Div class="centerX2 z4"><img class="imgcont2" src="http://localhost/Clubauto/Media/entretien.webp" alt="technicien qui nettoie un véhicule avec précision, le regard dans le prolongement du véhicule"></Div>
+            
         </section>
 
-        <section> <!--2eme section background et 2 conteneurs-->
+        <section class="imgsec2 Tsec2"> <!--2eme section background et 2 conteneurs-->
             <Div class="bgorange">
                 <h3>Vous souhaitez louer ?</h3>
                 <p class="ftwhite">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ratione asperiores quidem aperiam deserunt incidunt, eligendi ad? Magnam dolorem fugiat eum in? Eaque, laudantium accusantium.</p>
@@ -107,7 +107,7 @@
             </div>
         </section>
 
-        <section><!--4 eme section  1/4/1 conteneur -->
+        <section class="">
 
             <div><h2>Pourquoi nous choisir ?</h2></div>
             
@@ -148,7 +148,7 @@
 
         </section>
 
-        <section> <!--5eme section 1 conteneur avec transparence et background en fond-->
+        <section  class="imgsec3 Tsec2"> <!--5eme section 1 conteneur avec transparence et background en fond-->
             <div class="bgorange op">
                 <h2>CLUB AUTO<br><span class="ftwhite">VENTE ET LOCATION</span></h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur et vel eaque. Fugit vitae inventore architecto alias velit totam magnam sint dignissimos vero quis, necessitatibus porro cumque, corrupti culpa voluptates? Est qui deleniti nemo cum!</p>
